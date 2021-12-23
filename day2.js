5424d03cd305fd0df20a9ef180d819f32851db9d
nishapindoriya <96564126+nishapindoriya@users.noreply.github.com>
2021-12-23 16:03:33 +0530

    updated Day2 file

--- day2.js
+++ day2.js
@@ -46,6 +46,7 @@ console.log(arrayOfNumbers);
 
 var numberArray =[[1,2,3,4] , [5,6,7,8] , [9,10,11,12],[[13,14,15,16],17,18]];
  numberArray[1][4] =5;
+ console.log("Show number of array");
 console.log(numberArray);
 
 
